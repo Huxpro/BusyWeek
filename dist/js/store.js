bbff81649363213eb96b0e31efb900fe098652ee
鬼栈 <huangxuan.hx@alibaba-inc.com>
2015-01-04 19:30:41 +0800

    grunt build

--- dist/js/store.js
+++ dist/js/store.js
@@ -1,2 +1,2 @@
-/*! busyWeek 2014-12-19 */
+/*! busyWeek 2015-01-04 */
 !function(a){"use strict";var b="busyWeek";a.todoStorage={fetch:function(){return JSON.parse(localStorage.getItem(b)||"{}")},save:function(a){localStorage.setItem(b,JSON.stringify(a))}}}(window);
