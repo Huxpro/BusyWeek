335a670cb44c1da5341164811405daf73293872d
鬼栈 <huangxuan.hx@alibaba-inc.com>
2015-05-02 18:49:16 +0800

    a big update! nice trying

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// filter.js
+// filters
 Vue.filter('getDay', function (_dateStr) {
     var _date = new Date(_dateStr),
         _day = _date.getDay(),
@@ -18,12 +18,17 @@ Vue.filter('getDayType', function (_dateStr) {
     if (_dayMap[_diff]) {
         return _dayMap[_diff];
     } else {
-        return _diff > 0 ? (_diff+"天后") : (-_diff+"天前");
+        return _diff > 0 ? (_diff + "天后") : (-_diff + "天前");
     }
 
 });
 
 
+// State Enum
+var states = {
+    INPUT : "INPUT",
+    LIST  : "LIST"
+}
 
 
 // app.js
@@ -31,69 +36,79 @@ var app = new Vue({
     el: '#app',
     data: {
         appName: "BusyWeek!",
-        loaded:false,
-        inputMode: true,
-        flagDate:'',
+        loaded: false,
+        state:states.LIST,
         newTodo: {
-            dayType:'0',
+            dayType: '0',
             date: getTodayDate(),
             text: '',
             done: false
         },
         _todos: [{
-            text: '这个是示例哦～',
+            text: '这个只是代码示例哦～',
             done: false
         }],
-        _day:{
-            dayType:'0', // 没用，因为需要渲染时 update 
+        _day: {
+            dayType: '0', // 没用，因为需要渲染时 update 
             date: getTodayDate(),
             todos: this._todos
         },
-        timeline:todoStorage.fetch()
+        timeline: todoStorage.fetch()
     },
+    
     // Lifecycle created
-    created: function(){
+    created: function () {
         // detect language
         var _nav = navigator;
-        var _lang = (_nav.language || _nav.browserLanguage || _nav.userLanguage || "").substr(0,2);
+        var _lang = (_nav.language || _nav.browserLanguage || _nav.userLanguage || "").substr(0, 2);
 
-        if (_lang == "zh"){
+        if (_lang == "zh") {
             this.appName = "好忙啊";
         }
     },
+    
     // Lifecycle ready 
     ready: function () {
-        // locStorage _timeline
+        // store _timeline
         this.$watch('timeline', function (_timeline) {
             //console.log(JSON.stringify(_timeline));
             todoStorage.save(_timeline);
         }, true);
 
         // vue 11+ 解决了死循环 watch 的稳定问题 (oldVal = newVal && break;)
-        this.$watch('newTodo.dayType',function(_dayType, _dayType_old){
+        this.$watch('newTodo.dayType', function (_dayType, _dayType_old) {
 
             var _dateStr = getDiffDate(_dayType);
             this.newTodo.date = _dateStr;
         });
-        this.$watch('newTodo.date',function(_date, _date_old){
+        this.$watch('newTodo.date', function (_date, _date_old) {
 
             var _diff = getDateDiff(_date, getTodayDate());
-            if(_diff >= 0 && _diff<=7){
+            if (_diff >= 0 && _diff <= 7) {
                 this.newTodo.dayType = String(_diff);
             }
         });
-
     },
+    
     methods: {
-        //loading
-        onLoaded: function(e){
+
+        onLoaded: function (e) {
             this.loaded = true;
         },
+        
+        onActionAdd: function(e){
+            console.log(this.state);
+            this.state = states.INPUT;
+             console.log(this.state);
+        },
+        
         // todo
         addTodo: function (e) {
-            e.preventDefault()    
+            e.preventDefault()
+            
+            this.state = states.LIST;
 
-            if(!this.newTodo.text){
+            if (!this.newTodo.text) {
                 this.newTodo.text = "写点啥呀！"
             }
 
@@ -102,10 +117,10 @@ var app = new Vue({
                 _timeline = this.timeline;
 
             // new dayObject in timeline
-            if(!_timeline[_date]){
-                _timeline.$add(_date,{
+            if (!_timeline[_date]) {
+                _timeline.$add(_date, {
                     date: _date,
-                    todos:[]
+                    todos: []
                 })
             }
 
@@ -114,8 +129,8 @@ var app = new Vue({
 
             // reset newTodo
             this.newTodo = {
-                dayType:'0',
-                date : _date,
+                dayType: '0',
+                date: _date,
                 text: '',
                 done: false
             }
@@ -132,23 +147,23 @@ var app = new Vue({
                 this.timeline.$delete(todo.date);
             }
         },
-        checkTodo: function(todo){
+        checkTodo: function (todo) {
             todo.done = !todo.done;
         },
-        editTodo: function(todo){
+        editTodo: function (todo) {
 
         },
-        hackCheckbox: function(e){
+        hackCheckbox: function (e) {
             return false;
         },
-        enterInputMode: function(e){
+        enterInputMode: function (e) {
             //this.inputMode = true;
         },
-        exitInputMode: function(e){
+        exitInputMode: function (e) {
             //this.inputMode = false;
         },
-        ifToday: function(_dateStr){
-            if(_dateStr == getTodayDate()){
+        ifToday: function (_dateStr) {
+            if (_dateStr == getTodayDate()) {
                 return true;
             }
             return false;
@@ -157,45 +172,48 @@ var app = new Vue({
     }
 })
 
+
+
+
 /* Util Method */
 
-function getDateStr(_date){
+function getDateStr(_date) {
     var _y = _date.getFullYear(),
-        _m = _date.getMonth()+1,    //  好坑，从0开始
-        _m = _m < 10 ? "0"+_m : _m,
+        _m = _date.getMonth() + 1,    //  好坑，从0开始
+        _m = _m < 10 ? "0" + _m : _m,
         _d = _date.getDate(),
-        _d = _d < 10 ? "0"+_d : _d,
-        _s = _y+"-"+_m+"-"+_d;
+        _d = _d < 10 ? "0" + _d : _d,
+        _s = _y + "-" + _m + "-" + _d;
 
     return _s;
 }
 //返回今天的日期格式化字符串
-function getTodayDate(){
+function getTodayDate() {
     var _date = new Date();
-    
+
     return getDateStr(_date);
 }
 
 //计算两个日期的相差天数
-function getDateDiff(startDate, endDate){
+function getDateDiff(startDate, endDate) {
 
-   var aDate, oDate1, oDate2, iDays ;
-   aDate = startDate.split('-');
-   oDate1 = new Date(aDate[0]+'-'+aDate[1]+'-'+aDate[2]) ;
-   aDate = endDate.split('-');
-   oDate2 = new Date(aDate[0]+'-'+aDate[1]+'-'+aDate[2]);
-   iDays = parseInt((oDate1 -oDate2)/1000/60/60/24); //把相差的毫秒数转换为天数
+    var aDate, oDate1, oDate2, iDays;
+    aDate = startDate.split('-');
+    oDate1 = new Date(aDate[0] + '-' + aDate[1] + '-' + aDate[2]);
+    aDate = endDate.split('-');
+    oDate2 = new Date(aDate[0] + '-' + aDate[1] + '-' + aDate[2]);
+    iDays = parseInt((oDate1 - oDate2) / 1000 / 60 / 60 / 24); //把相差的毫秒数转换为天数
 
-   return iDays ;
+    return iDays;
 }
 
 // 返回目标天的日期格式化字符串
-function getDiffDate(_dayType){
-    var _time = new Date().getTime()+ _dayType*24000*3600,
+function getDiffDate(_dayType) {
+    var _time = new Date().getTime() + _dayType * 24000 * 3600,
         _date = new Date();
 
     _date.setTime(_time);
 
-    return getDateStr(_date);; 
+    return getDateStr(_date);;
 }
 
