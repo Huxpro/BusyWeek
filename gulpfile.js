1a539351732e73ef8501048459f180521effec35
黄玄 <huxpro@gmail.com>
2015-06-10 00:35:40 +0800

    use Gulp-Sass & Autoprefixer to build CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 /**
  * Created by 黄玄 on 15/06/02.
+ * Updated by Hux on 15/06/10.
  * 
  * Gulpfile 用于新版 requireJS 方案打包
  */
@@ -16,15 +17,27 @@ gulp.task('serve', function() {
     });
 });
 
-// compile scss
+// compile sass
+var sass = require('gulp-sass');
 gulp.task('sass', function() {
-    exec(['sass src/css/style.scss src/css/style.css'].join(' '), function(error, stdout, stderr) {
-        console.log('finish build sass');
-    });
+    return gulp.src('./src/css/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./src/css/'))
 });
 
+// PostCSS
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer-core');
+gulp.task('prefix',['sass'], function () {
+    return gulp.src('./src/css/style.css')
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+        .pipe(gulp.dest('./src/css/')); // 修改源文件
+});
+
+
+
 // build
-gulp.task('build', ['sass'], function() {
+gulp.task('build', ['sass', 'prefix'], function() {
     var build = 'node r.js -o build.js';
     
     exec(build, function callback(error, stdout, stderr){
@@ -32,6 +45,9 @@ gulp.task('build', ['sass'], function() {
         console.log('finish build');
     });
 });
+
+
+
 
 // default
 gulp.task('default', ['serve']);
