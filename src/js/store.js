204a768c134e2d5144e78f864a9dcd80c8a9b23f
黄玄 <huxpro@gmail.com>
2015-06-10 00:39:09 +0800

    本来想改数据结构，后来发现没必要，方法留着

--- src/js/store.js
+++ src/js/store.js
@@ -9,12 +9,47 @@
 
     exports.todoStorage = {
         fetch: function () {
-            // modified to {} from [] by Hux
+            // for debug use. 
+            window.data = JSON.parse(localStorage.getItem(STORAGE_KEY)); 
+                
             return JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}');
         },
-        save: function (todos) {
-            localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+        save: function (data) {
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
         }
     };
 
+    /**
+     * Polyfill to the isArray()
+     */
+    function isArray(o) {        
+        if (!Array.isArray) {
+            Array.isArray = function(arg) {
+                return Object.prototype.toString.call(arg) === '[object Array]';
+            };
+        }
+        return Array.isArray(o);
+    }
+    
+    /**
+     * Data Structure Forward Compatible
+     * 
+     * Created by @Hux at 2015/06/09
+     * Unused by @Hux at 2015/06/10
+     * Someday it will be used
+     * 
+     */
+    function ConvertData(data){
+        
+        // timeline = {} 
+        if (typeof data == "object" && !isArray(data)) {
+            console.log("Old Struct: Timeline is a Object");
+        }
+        
+        // timeline = [];
+        if (typeof data == "object"  && isArray(data)) {
+            console.log("Timeline is a Array, get its version");
+        }
+    }
+   
 })(window);
