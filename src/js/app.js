a99502cbb31cdc36ab8ce02b5061dba420661a55
黄玄 <huxpro@gmail.com>
2015-07-12 22:29:35 +0800

    optimize for cordova, support npm script

--- src/js/app.js
+++ src/js/app.js
@@ -31,14 +31,14 @@ require( [
     'util',
     'nav'
 ], function(
-    Vue, 
+    Vue,
     IScroll,
     FastClick,
     Router,
     AV,
     todoStorage,
     Util
-) {   
+) {
     'use strict';
     console.log("module app loaded..");
 
@@ -47,22 +47,22 @@ require( [
         var _date = new Date(_dateStr),
             _day = _date.getDay(),
             _dayMap = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
-    
+
         return _dayMap[_day];
     });
-    
+
     Vue.filter('getDayType', function (_dateStr) {
         var _diff = Util.getDateDiff(_dateStr, Util.getTodayDate()),
             _dayMap = ["今天", "明天", "后天", "大后天", "第五天", "第六天", "第七天", "下周今天"];
-        
+
         if (_dayMap[_diff]) {
             return _dayMap[_diff];
         } else {
             return _diff > 0 ? (_diff + "天后") : (-_diff + "天前");
         }
     });
-    
-    
+
+
     /**
      * App 的全局状态
      *
@@ -72,12 +72,12 @@ require( [
         INPUT : "INPUT",
         LIST  : "LIST"
     }
-    
+
     /**
-     * App 
-     * 
+     * App
+     *
      * app.js
-     */    
+     */
     var app = new Vue({
         el: '#app',
         data: {
@@ -85,7 +85,7 @@ require( [
             standalone: false,
             loaded: false,
             state:states.LIST,
-            
+
             /**
              * 登录 类
              *
@@ -94,7 +94,7 @@ require( [
             login: {
                 state: 'default',
                 tips: '如果账号不存在，系统会自动为您完成注册',
-                username: '',   
+                username: '',
                 password: '',
                 disabled: false,
                 logined: false,
@@ -112,21 +112,21 @@ require( [
                 done: false,
                 text: ''
             },
-            
+
             editingTodo: null,
-            
+
             /**
              * 时间轴 Map 对象
              *
-             * @object Timeline  { date: {DayObject} }        
+             * @object Timeline  { date: {DayObject} }
              * @object DayObject { date: {Date}, todos: [{Todo}] }
              * @object Todo      { date: {Date}, dayType: {Number}, done: {Bool}, text:{String}}
              */
             timeline: todoStorage.fetch(),
-            
+
             /**
              * 当前过滤器
-             * 
+             *
              * @param {string} all
              * @param {string} done
              * @param {string} active
@@ -140,11 +140,11 @@ require( [
             this.initRouter();
         },
         ready: function () {
-            
+
             this.initScroll();
             this.isStandAlone();
-            
-            
+
+
 
             // watch filter to refresh IScroll
             this.$watch('activeFilter', function(_filter){
@@ -158,34 +158,34 @@ require( [
                 todoStorage.save(_timeline);
                 this.refreshScroll();
             }, true);
-    
+
             // vue 11+ 解决了循环 watch 的稳定问题 (oldVal = newVal && break;)
             this.$watch('newTodo.dayType', function (_dayType, _dayType_old) {
-                
+
                 if( _dayType == "8"){
                     document.querySelector('.bw-datepicker').focus();
                 }
-                
+
                 var _dateStr = Util.getDiffDate(_dayType);
                 this.newTodo.date = _dateStr;
             });
-            
+
             this.$watch('newTodo.date', function (_date, _date_old) {
                 var _diff = Util.getDateDiff(_date, Util.getTodayDate());
                 console.log(_diff);
-                
+
                 if (_diff >= 0 && _diff <= 7) {
                     this.newTodo.dayType = String(_diff);
                 }else {
                     this.newTodo.dayType = "8";
                 }
             });
-            
-            // App.loaded 
+
+            // App.loaded
             console.log("vue working...");
             this.loaded = true;
         },
-        
+
         // supported by Vue-TodoMVC
         directives: {
             'todo-focus': function (value) {
@@ -193,38 +193,38 @@ require( [
                     return;
                 }
                 var el = this.el;
-                Vue.nextTick(function(){ 
+                Vue.nextTick(function(){
                     el.focus();
                 })
             }
         },
-        
+
         methods: {
-            
+
             onActionAdd: function(e){
                 this.state = (this.state == "LIST" ) ? states.INPUT : states.LIST;
             },
-            
-            // Todo 
+
+            // Todo
             addTodo: function (e) {
                 e.preventDefault()
-                
+
                 var _new = this.newTodo,
                     _date = _new.date,
                     _dayType = _new.dayType,
-                    _timeline = this.timeline; 
-                
+                    _timeline = this.timeline;
+
                 // change state and update view
                 this.state = states.LIST;
-                
+
                 // scroll to Todo
                 this.scrollToTodo(_timeline, _date);
-                
-                // set default todo text 
+
+                // set default todo text
                 if (!this.newTodo.text) {
                     this.newTodo.text = "写点啥呀！"
                 }
-    
+
                 // create new dayObject in timeline
                 if (!_timeline[_date]) {
                     _timeline.$add(_date, {
@@ -232,23 +232,23 @@ require( [
                         todos: []
                     })
                 }
-    
+
                 // push newTodo to corresponding dayObject
                 _timeline[_date].todos.push(this.newTodo);
-    
+
                 // reset newTodo
                 this.newTodo = {
                     dayType: _dayType,  // save dayType
-                    date: _date,        // save date 
+                    date: _date,        // save date
                     text: '',
                     done: false
                 }
-    
+
             },
             removeTodo: function (todo) {
                 // remove todo
                 this.timeline[todo.date].todos.$remove(todo.$data);
-            
+
                 // if day empty
                 if (this.timeline[todo.date].todos.length == 0) {
                     this.timeline.$delete(todo.date);
@@ -290,7 +290,7 @@ require( [
                 }else{
                     return true;
                 }
-                
+
             },
             // if day show in activeFilter
             ifDayShow: function(todos){
@@ -301,11 +301,11 @@ require( [
                 var activeTodos = todos.filter(function(todo){
                     return !todo.done
                 })
-                
+
                 // 完成数与活动数
                 var actives = activeTodos.length;
                 var dones = todos.length - actives;
-               
+
                 // 只要有未完成的，今天就需要显示
                 if (this.activeFilter == "active"){
                     if(actives > 0){
@@ -313,7 +313,7 @@ require( [
                     }else{
                         return false;
                     }
-                
+
                 // 只要有完成的，今天就需要展示
                 }else if(this.activeFilter == "done"){
                     if (dones > 0) {
@@ -324,7 +324,7 @@ require( [
                 }else{
                     return true;
                 }
-               
+
             },
             ifAboutShow: function(){
                 return this.activeFilter == "about";
@@ -342,7 +342,7 @@ require( [
             detectLanguage: function(){
                 var _nav = navigator;
                 var _lang = (_nav.language || _nav.browserLanguage || _nav.userLanguage || "").substr(0, 2);
-            
+
                 if (_lang == "zh") {
                     this.appName = "好忙啊";
                 }
@@ -365,20 +365,20 @@ require( [
             },
             // IScroll function
             initScroll: function(){
-                this.BWScroll = new IScroll('.bw-scroller', { 
-                    mouseWheel: true, 
+                this.BWScroll = new IScroll('.bw-scroller', {
+                    mouseWheel: true,
                     bindToWrapper: true,
                     click: iScrollClick(),
                     //tap: iScrollClick(),
                     //preventDefault:  iScrollClick(),
-                    
+
                 });
 
                 function iScrollClick(){
                     if (/iPhone|iPad|iPod|Macintosh/i.test(navigator.userAgent)) return false;
                     if (/Chrome/i.test(navigator.userAgent)) return (/Android/i.test(navigator.userAgent));
                     if (/Silk/i.test(navigator.userAgent)) return false;
-                    if (/Android/i.test(navigator.userAgent)) 
+                    if (/Android/i.test(navigator.userAgent))
                     {
                       var s=navigator.userAgent.substr(navigator.userAgent.indexOf('Android')+8,3);
                       return parseFloat(s[0]+s[3]) < 44 ? false : true
@@ -389,26 +389,32 @@ require( [
                 var self = this;
 
                 if(this.BWScroll){
-                    Vue.nextTick(function(){ 
+                    Vue.nextTick(function(){
                         self.BWScroll.refresh();
                         setTimeout(function () {
                             self.BWScroll.refresh();
                         }, 300);
-                    })  
+                    })
                 }
             },
             // standalone
             isStandAlone: function(){
                 var _isIPhone = navigator.userAgent.indexOf('iPhone') != -1;
                 var _isStandalone = window.navigator.standalone == true;
-                
-                if( _isIPhone && _isStandalone ){    
+                var _isBrowser = document.URL.match(/^https?:/)
+
+                if(!_isBrowser){
+                    this.standalone = true;
+                    return;
+                }
+
+                if( _isIPhone && _isStandalone  ){
                     this.standalone = true
-                }                 
+                }
             },
             /**
              * calculate the newTodo position and Scroll to!
-             */ 
+             */
             scrollToTodo: function(_timeline, _date){
                 // 把 timeline.key 做一次排序
                 var sorted = (function(){
@@ -420,7 +426,7 @@ require( [
                         return a === b ? 0 : a > b ? 1 : -1
                     })
                 })()
-                
+
                 // 拿到 _date 之前的 key
                 var before = (function(){
                     var arr = [];
@@ -431,42 +437,42 @@ require( [
                     })
                     return arr;
                 })()
-                
+
                 // 根据之前的 key 算出 items 数和位置
                 var position = (function(){
                     var day = before.length,
                         items = 0;
-                    
+
                     before.forEach(function (date) {
                         var dayList = _timeline[date];
                         items += dayList.todos.length;
-                    }) 
-                    
+                    })
+
                     //console.log("day:"+day+" items:"+items);
-                    
+
                     return day*36+(items+7)*48;
                 })()
-                
+
                 // 根据位置算出 ScrollY
                 var scrollY = (function(){
                     var edge = window.innerHeight || window.screen.availHeight;
-                    
+
                     if(position < edge){
                         return 0;
                     }else{
                         return position - edge;
                     }
                 })()
-                
+
                 // jump to the target Scroll Position at NEXT View Update
                 // this method falls back to setTimeout(fn, 0)
                 var _scroll = this.BWScroll;
 
-                Vue.nextTick(function(){ 
+                Vue.nextTick(function(){
                     //scroll(0, scrollY);
                     _scroll.scrollTo(0, -scrollY)
                 })
-                
+
         //      console.log(before);
         //      console.log("position:"+ position);
         //      console.log("scrollTo:"+ scrollY);
@@ -597,23 +603,20 @@ require( [
             }
         }
     })
-    
+
 
     // export app to global
     window.vue = Vue;
     window.app = app;
-    
+
     // deal with load
     //app.loaded = true;
-    
+
     // FastClick
     FastClick.attach(document.body);
     document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
     console.log("fastclick working...");
-    
-        
-    
+
+
+
 });
-
-
-
